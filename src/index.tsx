19ffc3630fc01314ad1ab1457f7f279f41942a47
junho34240616-alt <junho34240616@gmail.com>
2026-03-18 20:52:56 +0900

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -297,7 +297,17 @@ function baseHead(title: string, extraHead = '') {
 
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').catch(function() {});
+        navigator.serviceWorker.getRegistrations().then(function(registrations) {
+          return Promise.all(registrations.map(function(registration) {
+            return registration.unregister();
+          }));
+        }).then(function() {
+          return caches.keys().then(function(keys) {
+            return Promise.all(keys.map(function(key) {
+              return caches.delete(key);
+            }));
+          });
+        }).catch(function() {});
       });
     }
   </script>
